6d3195e0dfa31efb8fe1fcb5af2a17cc5d1ce9d2
Sumit Arora <er.sumitarora@gmail.com>
2016-08-24 15:47:42 -0400

    adding log

--- api/Services/apiaiservice.js
+++ api/Services/apiaiservice.js
@@ -1,22 +1,24 @@
 var apiai = require('apiai');
 var app = apiai("4505c8543b8045059c362be1864f1d19");
 var q = require('q');
- 
-function getIntent (message){
-	var request = app.textRequest(message);
-	var deferred = q.defer();
 
-	request.on('response', function(response) {
-		deferred.resolve(recipe.result.url);
-	});
+function getIntent(message) {
+  var request = app.textRequest(message);
+  var deferred = q.defer();
 
-	request.on('error', function(error) {
-		deferred.reject(error);
-	});
- 
-	request.end();
-	return deferred.promise;
-} 
+  request.on('response', function(response) {
+    console.log(response);
+    deferred.resolve(recipe.result.url);
+  });
+
+  request.on('error', function(error) {
+    console.log(error);
+    deferred.reject(error);
+  });
+
+  request.end();
+  return deferred.promise;
+}
 
 
 
